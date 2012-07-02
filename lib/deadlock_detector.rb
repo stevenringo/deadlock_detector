aff100191edc5d7f242f5c80719e39f6dbd266e0
Steven Ringo <git@stevenringo.com>
2012-07-02 13:02:17 +1000

    Tie together in an executable file

--- lib/deadlock_detector.rb
+++ lib/deadlock_detector.rb
@@ -1,5 +1,17 @@
 require "deadlock_detector/version"
+require "deadlock_detector/graph_converter"
+require "deadlock_detector/directed_graph"
 
 module DeadlockDetector
-  # Your code goes here...
+  class Detector
+    def self.validate(file)
+      lines = File.open(file,'r').read
+      input_array = []
+      lines.each_line do |line|
+        input_array << line.chomp.split(",")
+      end
+      directed_graph = GraphConverter.convert(input_array)
+      DirectedGraph.new.has_loops?(directed_graph) ? "BAD" : "GOOD"
+    end
+  end
 end
